bd34566858765b3da340e11efacfca2e682ecb13
Jashwanth S Poojary <149219296+JashwanthSPoojary@users.noreply.github.com>
2025-06-30 14:54:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   const [sidebar,setsidebar] = useState(true);
   return (
     <div>
-{/*       <Navbar setsidebar={setsidebar}/>*/}
+       <Navbar setsidebar={setsidebar}/>
       <Routes > 
         <Route path='/' element={<Home sidebar={sidebar}/>}/>
         <Route path='/video/:categoryId/:videoId' element={<Video/>}/>
